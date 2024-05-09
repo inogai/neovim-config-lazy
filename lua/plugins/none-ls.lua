128cd764e90299c2a69d891aff6d05de401d63c8
inogai <inogai.sign@gmail.com>
2024-05-09 17:45:53 +0800

    [none-ls] stylua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,10 @@ return {
     },
 
     opts = function(_, opts)
+      local null_ls = require("null-ls")
+
       opts.sources = vim.list_extend(opts.sources or {}, {
+        null_ls.builtins.formatting.stylua,
         require("none-ls-shellcheck.diagnostics"),
         require("none-ls-shellcheck.code_actions"),
       })
