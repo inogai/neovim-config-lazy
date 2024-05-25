38731d730b6c0461e5804ba74b3444bc7826efd1
inogai <inogai.sign@gmail.com>
2024-05-25 15:23:48 +0800

    [options] relative numbering

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 local function __diffview_detector_recursive(bufnr, depth)
   local bufname = vim.fn.bufname(bufnr)
