5d2cb18b494f3176bcca736c7a3baea729c7b3c4
inogai <inogai.sign@gmail.com>
2024-06-25 22:53:11 +0800

    feat(nvim-cmp): tab once to complete first, twice to 2nd

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,9 +3,15 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
+
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+
+      opts.preselect = cmp.PreselectMode.None
+
       opts.mapping = {
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab><Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       }
     end,
