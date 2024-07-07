4993dc421e27539fa60ed8375dd963bb6741307b
inogai <inogai.sign@gmail.com>
2024-07-07 19:55:21 +0800

    feat(leetcode): leetcode plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      injector = { ---@type table<lc.lang, lc.inject>
+        ["python3"] = {
+          before = true,
+        },
+        ["cpp"] = {
+          before = { "#include <iostream>", "#include <vector>", "using namespace std;" },
+          after = "int main() {}",
+        },
+      },
+    },
+  },
+}
