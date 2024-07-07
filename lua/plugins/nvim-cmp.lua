83e457aedc4a417c602aec3bb627bc4545191f91
inogai <inogai.sign@gmail.com>
2024-07-07 19:04:17 +0800

    feat(nvim-cmp): update keymaps

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,8 +8,24 @@ return {
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = true })
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.snippet.jump(1)
+          else
+            fallback()
+          end
+        end,
+        ["<S-Tab>"] = function(fallback)
+          if vim.snippet.active({ direction = -1 }) then
+            vim.snippet.jump(-1)
+          -- elseif cmp.visible() then
+          --   cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end,
       }
     end,
   },
