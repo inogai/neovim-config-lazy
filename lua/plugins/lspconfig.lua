87c1a239440ba830deb570cf5d6d6f37051c1ed1
inogai <inogai.sign@gmail.com>
2024-07-22 23:20:18 +0800

    feat: lspconfig for clangd and vtsls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,5 +4,33 @@ return {
     inlay_hints = {
       enabled = true,
     },
+    servers = {
+      clangd = {
+        cmd = {
+          "/usr/bin/clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+      },
+
+      vtsls = {
+        settings = {
+          typescript = {
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = true },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = true },
+            },
+          },
+        },
+      },
+    },
   },
 }
