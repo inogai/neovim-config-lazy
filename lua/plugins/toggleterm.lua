a418d8204e34abd9155349821ac0c97a38fcdc6a
inogai <inogai.sign@gmail.com>
2024-07-22 23:18:08 +0800

    feat(toggleterm): config for multiple terms

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,16 +1,27 @@
+local function toggle()
+  local toggleterm = require("toggleterm")
+  local cnt = vim.v.count
+
+  if cnt == 0 then
+    toggleterm.toggle(1, 8, LazyVim.root())
+  else
+    toggleterm.toggle(cnt, 8, LazyVim.root())
+  end
+end
+
 return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    config = true,
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+    end,
     cmd = { "ToggleTerm", "TermExec" },
     -- stylua: ignore
     keys = {
-      { "<c-/>", function ()
-        require("toggleterm").toggle(1,8, LazyVim.root())
-      end, desc = "ToggleTerm" },
+      { "<c-/>", toggle },
+      { "<c-\\>", function() require("toggleterm").toggle_all() end },
     },
-
     opts = {
       shade_terminals = false,
     },
