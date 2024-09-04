cd757faf31cfcd2541dcfbce0b1e0a4cd62b17e4
inogai <inogai.sign@gmail.com>
2024-09-04 14:44:00 +0800

    feat: prepare rainbow-delim hl groups if not provided

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -20,8 +20,30 @@ return {
   },
   {
     "HiPhish/rainbow-delimiters.nvim",
-
     config = function()
+      local default_colors = {
+        "#b5a165",
+        "#528f77",
+        "#8e7395",
+        "#5a7aaa",
+      }
+
+      function prepare_hl_groups()
+        local rd1_id = vim.api.nvim_get_hl(0, { name = "RainbowDelimiter1", create = false })
+
+        if rd1_id == 0 then
+          for i, color in ipairs(default_colors) do
+            vim.nvim_set_hl(0, "RainbowDelimiter" .. i, { guifg = color })
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = prepare_hl_groups,
+      })
+
+      prepare_hl_groups()
+
       ---@module "rainbow-delimiters"
       ---@type rainbow_delimiters.config
       vim.g.rainbow_delimiters = {
