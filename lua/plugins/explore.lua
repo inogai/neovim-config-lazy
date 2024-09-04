e5c3d5f301b2d524cdfa84fd657307edde5ad445
inogai <inogai.sign@gmail.com>
2024-09-04 16:07:10 +0800

    feat(explore): ref toggle root & '@' to focus current file

--- lua/plugins/explore.lua
+++ lua/plugins/explore.lua
@@ -1,8 +1,65 @@
-local minifiles_toggle = function()
+local M = {}
+
+---@param cwd string
+function M.toggle(cwd)
   local MiniFiles = require("mini.files")
   if not MiniFiles.close() then
-    MiniFiles.open(LazyVim.root(), true)
+    MiniFiles.open(cwd, true)
   end
+end
+
+function M.toggle_at_root()
+  M.toggle(LazyVim.root())
+end
+
+---@type integer[]
+M.MF_BUFS = {}
+
+---@param cwd string
+function M.focus(cwd)
+  local MiniFiles = require("mini.files")
+  MiniFiles.open(cwd, false)
+end
+
+---@param blacklist integer[]
+function M.find_last_active_file(blacklist)
+  local bufs = vim.api.nvim_list_bufs()
+  local last_active_dir = nil
+  local last_active_time = 0
+
+  for _, buf in ipairs(bufs) do
+    if vim.tbl_contains(blacklist, buf) then
+      goto continue
+    end
+
+    local bufinfo = vim.fn.getbufinfo(buf)[1]
+
+    if not vim.fn.filereadable(bufinfo.name) then
+      goto continue
+    end
+
+    if bufinfo.lastused > last_active_time then
+      last_active_time = bufinfo.lastused
+      last_active_dir = bufinfo.name
+    end
+
+    ::continue::
+  end
+
+  return last_active_dir
+end
+
+function M.foucs_last_active_file_dir()
+  local laf = M.find_last_active_file(M.MF_BUFS)
+
+  if laf == nil then
+    print("No dir to be focused.")
+    return
+  end
+
+  local last_active_dir = vim.fn.fnamemodify(laf, ":p:h")
+
+  M.focus(last_active_dir)
 end
 
 return {
@@ -13,6 +70,7 @@ return {
     opts = function(_, opts)
       opts.mappings = {
         toggle_hidden = "g.",
+        reveal_cwd = "",
         change_cwd = "gc",
         go_in_horizontal = "<C-w>s",
         go_in_vertical = "<C-w>v",
@@ -23,10 +81,18 @@ return {
       opts.options = {
         use_as_default_explorer = true,
       }
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          table.insert(M.MF_BUFS, args.data.buf_id)
+          vim.keymap.set("n", "@", M.foucs_last_active_file_dir)
+        end,
+      })
     end,
     -- stylua: ignore
     keys = {
-      { "<leader>e", minifiles_toggle, desc = "Files" },
+      { "<leader>e", M.toggle_at_root, desc = "Files" },
     },
   },
 }
