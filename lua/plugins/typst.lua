67a9a58d5d258633626d300c5b48a5385009afdc
inogai <inogai.sign@gmail.com>
2024-12-05 22:11:13 +0800

    feat(typst): use tinymist and external typstyle, also bump typst preview

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -1,14 +1,50 @@
 return {
+  -- requires tinymist
   {
-    "kaarmu/typst.vim",
-    ft = "typst",
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tinymist",
+      },
+    },
+  },
+  -- add tinymist to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        tinymist = {
+          --- todo: these configuration from lspconfig maybe broken
+          single_file_support = true,
+          root_dir = function()
+            return vim.fn.getcwd()
+          end,
+          --- See [Tinymist Server Configuration](https://github.com/Myriad-Dreamin/tinymist/blob/main/Configuration.md) for references.
+          settings = {},
+        },
+      },
+    },
   },
   {
     "chomosuke/typst-preview.nvim",
-    ft = 'typst',
-    version = "0.1.*",
-    build = function()
-      require("typst-preview").update()
-    end,
+    ft = "typst",
+    version = "1.*",
+    opts = {
+      dependencies_bin = { ["tinymist"] = "tinymist" },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typst = { "typstyle" },
+      },
+    },
   },
 }
