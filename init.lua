41eb052b18f03d2443ad13e6c1e588155b18dfc7
inogai <inogai.sign@gmail.com>
2024-12-22 22:26:45 +0800

    feat(neovide): grab focus on launch

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.g.neovide then
+      print("Neovide detected, setting focus")
+      vim.defer_fn(function()
+        vim.cmd("NeovideFocus")
+      end, 500)
+    end
+  end,
+})
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
