959a5fb34c73c9636f9c7f1ce93af27755ec8046
inogai <inogai.sign@gmail.com>
2024-12-24 00:13:10 +0800

    feat(heirline.Mode): revamp

--- lua/plugins/ui-heirline/Mode.lua
+++ lua/plugins/ui-heirline/Mode.lua
@@ -12,8 +12,8 @@ local mode_map = {
   ["niI"] = "NORMAL",
   ["niR"] = "NORMAL",
   ["niV"] = "NORMAL",
-  ["nt"] = "NORMAL",
-  ["ntT"] = "NORMAL",
+  ["nt"] = "N-TERMINAL",
+  ["ntT"] = "N-TERMINAL",
   ["v"] = "VISUAL",
   ["vs"] = "VISUAL",
   ["V"] = "V-LINE",
@@ -42,36 +42,60 @@ local mode_map = {
   ["t"] = "TERMINAL",
 }
 
-local _color_map = {
-  n = { fg = "fg3", bg = "bg1" },
-  i = { fg = "bg1", bg = "green" },
-  v = { fg = "bg1", bg = "purple" },
-  [""] = { fg = "bg1", bg = "purple" },
-  c = { fg = "bg1", bg = "yellow" },
-  t = { fg = "bg1", bg = "orange" },
-  ["*"] = { fg = "bg1", bg = "red" },
+---@class ColorMapItem
+---@field [1] string - the mode prefix
+---@field [2] HeirlineHighlight
+
+--- The `color_map` is a ordered list of mode to color mappings.
+--- the first match is used.
+---@type table<number, ColorMapItem>>
+local color_map = {
+  { "no", { fg = "bg1", bg = "orange" } },
+  { "nt", { fg = "bg1", bg = "orange" } },
+  { "n", { fg = "fg3", bg = "bg1" } },
+  { "i", { fg = "bg1", bg = "green" } },
+  { "v", { fg = "bg1", bg = "purple" } },
+  { "c", { fg = "bg1", bg = "yellow" } },
+  { "t", { fg = "bg1", bg = "purple" } },
+  { "", { fg = "bg1", bg = "red" } },
 }
 
-local color_map = function(mode)
-  return _color_map[mode] or _color_map["*"]
+---@param mode string
+---@return HeirlineHighlight
+local color_map_lookup = function(mode)
+  for _, v in pairs(color_map) do
+    if v[1] == mode then
+      return v[2]
+    end
+  end
+
+  -- fallback to the last item if no matches
+  return color_map[#color_map][2]
 end
 
+---@module "heirline"
+
 ---@class MyHeirline.ModeComponentSelf
----@field _color_map table<string, table<string, string>>
 ---@field mode mode
 ---@field _hl HeirlineHighlight
 
 local Mode = {
   ---@param self MyHeirline.ModeComponentSelf
   init = function(self)
-    self.mode = vim.fn.mode()
+    self.mode = vim.fn.mode(1)
   end,
 
-  update = { "ModeChanged" },
+  update = {
+    "ModeChanged",
+    pattern = "*:*",
+    callback = vim.schedule_wrap(function()
+      vim.cmd("redrawstatus")
+    end),
+  },
 
   ---@param self MyHeirline.ModeComponentSelf
   hl = function(self)
-    self._hl = color_map(self.mode)
+    self._hl = color_map_lookup(self.mode)
     return self._hl
   end,
 
@@ -86,7 +110,7 @@ local Mode = {
   {
     ---@param self MyHeirline.ModeComponentSelf
     provider = function(self)
-      if self.mode == "n" then
+      if mode_map[self.mode] == "NORMAL" then
         return symbols.PIN .. " "
       end
       return symbols.PIN .. " " .. mode_map[self.mode] or self.mode
@@ -99,7 +123,7 @@ local Mode = {
       return { fg = self._hl.bg, bg = "bg1" }
     end,
     provider = function(self)
-      if self.mode == "n" then
+      if mode_map[self.mode] == "NORMAL" then
         return ""
       end
       return symbols.RHC
