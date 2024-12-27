e9e428272b156f3e43e21a8910e3a7ce97eb80e1
inogai <inogai.sign@gmail.com>
2024-12-27 09:06:12 +0800

    feat(lang-python): use defaults for venv selector
    Signed-off-by: inogai <inogai.sign@gmail.com>

--- lua/plugins/lang-python.lua
+++ lua/plugins/lang-python.lua
@@ -8,32 +8,5 @@ return {
     },
     ft = "python",
     branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function(_, opts)
-      require("venv-selector").setup(opts)
-    end,
-    opts = function(_, opts)
-      opts = opts or {}
-
-      local function on_venv_activate()
-        local source = require("venv-selector").source()
-        local python = require("venv-selector").python()
-
-        if source == "miniconda" then
-          local conda_env_name =
-            python:gsub("^/opt/homebrew/Caskroom/miniconda/base/envs/", ""):gsub("/bin/python$", "")
-          local command = "conda activate " .. conda_env_name
-          require("toggleterm").exec(command, nil, nil, nil, nil, nil, nil, false)
-          return
-        end
-      end
-
-      opts.settings = opts.settings or {}
-      opts.settings.options.on_venv_activate_callback = on_venv_activate
-      opts.settings.search = opts.settings.search or {}
-      opts.settings.search.miniconda = {
-        command = [[conda env list | ggrep -v '^#' | ggrep . | gsed 's/^\\w\\+[ *]\\+//' | gsed 's/$/\\/bin\\/python/']],
-        type = "anaconda",
-      }
-    end,
   },
 }
