18a6db398df1832ab82616459110f9d2003680a9
inogai <inogai.sign@gmail.com>
2024-12-28 23:39:11 +0800

    ref(ui-heirline/prose): use opts

--- lua/plugins/ui-heirline/init.lua
+++ lua/plugins/ui-heirline/init.lua
@@ -68,14 +68,12 @@ end
 return {
   {
     "skwee357/nvim-prose",
-    config = function()
-      require("nvim-prose").setup({
-        placeholders = {
-          words = "",
-          minutes = "",
-        },
-      })
-    end,
+    opts = {
+      placeholders = {
+        words = "",
+        minutes = "",
+      },
+    },
   },
   {
     "SmiteshP/nvim-navic",
