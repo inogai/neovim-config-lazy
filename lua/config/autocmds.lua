8195b3e2310aaf14181c7eddea1816e496db0094
inogai <inogai.sign@gmail.com>
2024-12-29 14:20:52 +0800

    feat!: remove auto dashboard on last buf delete

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,36 +42,3 @@ autocmd("BufEnter", {
     vim.api.nvim_set_current_dir(cwd)
   end,
 })
-
--- source: @lukasx_ https://www.reddit.com/r/neovim/comments/1dymqgv/open_dashboard_after_last_buffer_has_been_closed
-autocmd("BufDelete", {
-  callback = function()
-    local buffers = vim.api.nvim_list_bufs()
-    local actual = {}
-
-    -- Filter out nofile buffers created by plugins
-    for i = 1, #buffers, 1 do
-      local current = buffers[i]
-      local buftype = vim.bo[current].buftype
-
-      if buftype ~= "nofile" then
-        table.insert(actual, current)
-      end
-    end
-
-    -- Return if theres more than 1 buffer
-    -- For some reason I see 1 extra buffer, so I added one for this
-    if #actual > 2 then
-      return
-    end
-
-    -- Get name of last buffer
-    -- And also this +1 too
-    local last_buf = actual[2]
-    local bufname = vim.api.nvim_buf_get_name(last_buf)
-
-    if bufname == "" then
-      vim.cmd("Dashboard")
-    end
-  end,
-})
